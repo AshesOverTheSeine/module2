de91d5590cfdd3467fff701a4eed667009d0e22e
AshesOverTheSeine <tysonsamuel93@gmail.com>
2016-06-08 08:49:40 -0700

    Finished but untested

--- module2/module2.ino
+++ module2/module2.ino
@@ -1,4 +1,4 @@
-// WHEEL PINS //
+d i th// WHEEL PINS //
 
 // Pins controlling wheel 1
 #define m1p1 3   // Forward
@@ -29,13 +29,22 @@
 #define G 4
 #define B 7
 
+// DISTANCE DETECTOR PINS //
+
+#define far A5
+#define near A4
 
 // VARIABLES //
 
 int speed = 255;  // Speed for the wheels to travel at
 int colour  = 1;  // Cycles between 1, 2, 3 (RGB)
+int count  = 0;   // Unique objects detected so far
 unsigned int red, green, blue = 0;  // Contains colour sensor values
+boolean reached = false;  // Keep track of if an object is seen/has been reached or not
 
+int movingAvgNear[10], movingAvgFar[10];
+int avgCount = 0;
+int avgFar, avgNear = 0;
 
 // The setup function runs once when you press reset or power the board
 void setup() {
@@ -67,23 +76,37 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  // Read from line sensor 1
-  int proximityADC1 = analogRead(LEFT);
-  float proximityV1 = (float)proximityADC1 * 5.0 / 1023.0;
 
-  // Read from line sensor 2
-  int proximityADC2 = analogRead(RIGHT);
-  float proximityV2 = (float)proximityADC2 * 5.0 / 1023.0;
+  // MOVING AND NAVIGATION //
+
+
+  // Have the vehicle slow as it approaches the object
+  if (avgNear > 500 && !reached)
+    speed = 100;
+  else
+    speed = 255;
 
   // Determine the direction to go - Turn if a line is encountered
-  if (proximityV1 <= 4.4 && proximityV2 > 4.4)
+  if (analogRead(LEFT) >= 900 && analogRead(RIGHT) < 900)
     left();
-  else if (proximityV1 > 4.4 && proximityV2 <= 4.4)
+  else if (analogRead(LEFT) < 900 && analogRead(RIGHT) >= 900)
     right();
-  else {
-    forward();
-    Serial.println(proximityV1);
-    Serial.println(proximityV2);
+  // Turn to the next spoke when the centre is reached
+  else if (analogRead(LEFT) >= 900 && analogRead(RIGHT) >= 900) {
+    // Tick off one object found
+    if (reached)
+      count++;
+
+    // Prepare to find the next object
+    reached = false;
+    scan();
+  // Car is aligned
+  } else {
+    // Go towards object upon finding it, else go back to the start
+    if (reached)
+      backward();
+    else
+      forward();
   }
 
 
@@ -114,44 +137,105 @@ void loop() {
     colour = 1;
   }
 
-  delay(100);
+  distance();
+  
+  // Long distance detection (all outdated, keeping for debugging)
+
+  /*
+  if (avgCount == 10) {
+    for (int i = 0; i < 10; i++)
+      avg += movingAvg[i];
+    //Serial.println(avg/10);
+
+    if (avg/10 > 70 && avg/10 <= 82)
+      high = 150;
+    else if (avg/10 <= 102)
+      high = 130;
+    else if (avg/10 <= 113)
+      high = 120;
+    else if (avg/10 <= 123)
+      high = 110;
+    else if (avg/10 <= 133)
+      high = 100;
+    else if (avg/10 <= 153)
+      high = 90;
+    else if (avg/10 <= 164)
+      high = 80;
+    else if (avg/10 <= 184)
+      high = 70;
+    else if (avg/10 <= 221)
+      high = 60;
+    else if (avg/10 <= 256)
+      high = 50;
+    else if (avg/10 <= 317)
+      high = 40;
+    else if (avg/10 <= 409)
+      high = 30;
+    else if (avg/10 <= 532)
+      high = 20;
+    else
+      Serial.println("Bad Reading");
+    
+    avgCount = 0;
+    avg = 0;
+  }
+
+
+    
+  movingAvg[avgCount] = analogRead(far);
+  avgCount++;
+  Serial.println(analogRead(near));
+*/
 
   // This is the easiest to detect. Red goes very low while green and blue go high
   if (red < 70 && green >= 100 && blue >= 100) {
     Serial.println("The sensor detects something that is Red.");
+    
     digitalWrite(R, HIGH);
     digitalWrite(G, LOW);
     digitalWrite(B, LOW);
 
-    // The hardest detection of all, all three values drop significantly, at roughly equal levels
-    // No one value seems particularly favored, any of them may be the lowest/highest
-    // The orange lights in the lab keep the red value low at default; with no environmental
-    // bias this would give too many false positives, need to be changed
+    reached = true;
+    
+  // The hardest detection of all, all three values drop significantly, at roughly equal levels
+  // No one value seems particularly favored, any of them may be the lowest/highest
+  // The orange lights in the lab keep the red value low at default; with no environmental
+  // bias this would give too many false positives, need to be changed
   } else if ((abs(blue - red) <= (blue * .15) && abs(blue - green) <= (blue * .15))
       || (abs(red - blue) <= (red * .15) && abs(red - green) <= (red * .15))
       || (abs(green - blue) <= (green * .15) && abs(green - red) <= (green * .15))) {
     Serial.println("The sensor detects something that is Green.");
+    
     digitalWrite(R, LOW);
     digitalWrite(G, HIGH);
     digitalWrite(B, LOW);
+    
+    reached = true;
 
-    // Even strong blues do not cause as much change as light red. 
-    // Red and green values do not go past 100, blue does not dip  very far
-    // Red and default will NEVER cause this to be true, green triggers before blue
-    // Reporting blue as green & vice versa does not occur with this ordering.
+  // Even strong blues do not cause as much change as light red. 
+  // Red and green values do not go past 100, blue does not dip  very far
+  // Red and default will NEVER cause this to be true, green triggers before blue
+  // Reporting blue as green & vice versa does not occur with this ordering.
   } else if (blue < red && blue < green) {
     Serial.println("The sensor detects something that is Blue.");
+  
     digitalWrite(R, LOW);
     digitalWrite(G, LOW);
     digitalWrite(B, HIGH);
+
+    reached = true;
+    
   } else {
     Serial.println("The sensor does not detect a strong Red, Green, or Blue.");
+  
     digitalWrite(R, LOW);
     digitalWrite(G, LOW);
     digitalWrite(B, LOW);
   }
 
   Serial.println("---");
+
+  // TODO: Might want a delay here
 
 }
 
@@ -191,4 +275,40 @@ void halt() {
   analogWrite(m2p1, speed);
   analogWrite(m2p2, speed);
 }
+
+void distance() {
+  // Keeps a moving average for the distance sensors as they can be a bit wild
+  movingAvgFar[avgCount] = analogRead(far);
+  movingAvgNear[avgCount] = analogRead(near);
+  avgCount = (avgCount + 1) % 10;
+  
+  for (int i = 0; i < 10; i++) {
+    avgFar += movingAvgFar[i];
+    avgNear += movingAvgNear[i];
+  }
+
+  avgFar = avgFar / 10;
+  avgNear = avgNear / 10;
+  
+}
+// Scans for the next object
+void scan() {
+  // Complete, do nothing
+  if (count == 3) {
+    halt();
+    delay(1000000000);
+  } else {
+    // Go forward if object detected. Having it travel up a bit so it doesn't have both sensors over tape
+    if (analogRead(far) > 125) {
+      forward();
+      delay(50);
+    // Otherwise, continue turning
+    } else {
+      right();
+      distance();
+      scan();
+    }
+  }
+}
+
 
